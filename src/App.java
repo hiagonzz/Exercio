d8508c5489456fda2d87f655c0f02bec2611b189
Hiago nunes pimentel <derundaclash665@gmail.com>
2023-11-20 09:47:02 -0300

    adionado classes employee, outsourceEmployee

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("hiago aqui, o lindo , o gostoso!");
+        System.out.println("hello");
     }
 }
--- src/entites/Employee.java
+++ src/entites/Employee.java
@@ -1,17 +1,51 @@
 package entites;
+
 public class Employee {
 
-        private String name;
-        private Integer hours;
-        private double  valuePerHour;
+    private String name;
+    private Integer hours;
+    private double valuePerHour;
 
-        Employee(){
+    Employee() {
 
-        }
-        Employee (String name , Integer hours, double valuePerHour){
-            this.name = name;
-            this.hours = hours;
-            this.valuePerHour = valuePerHour;
-        }
-    
+    }
+
+    Employee(String name, Integer hours, double valuePerHour) {
+        this.name = name;
+        this.hours = hours;
+        this.valuePerHour = valuePerHour;
+    }
+
+    // getters
+    public Integer getHours() {
+        return hours;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getValuePerHour() {
+        return valuePerHour;
+    }
+
+    // setters
+    public void setHours(Integer hours) {
+        this.hours = hours;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setValuePerHour(double valuePerHour) {
+        this.valuePerHour = valuePerHour;
+    }
+
+
+    public double payment(){
+        return hours*valuePerHour;
+
+    }
+
 }
